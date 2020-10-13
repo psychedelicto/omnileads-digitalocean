91a4d74eac55572a401e2ef0cd5538b06c4b454a
Fabian <fabian.pignataro84@gmail.com>
2020-10-13 17:49:09 -0300

    userdata rtpengine api IP

--- user_data/rtpengine.sh
+++ user_data/rtpengine.sh
@@ -8,7 +8,11 @@ systemctl disable firewalld
 
 yum install epel-release -y && yum install -y libpcap hiredis xmlrpc-c-client json-glib libevent http://freetech.com.ar/rpms/rtpengine-5.5.3.1-1.x86_64.rpm vim
 
-echo "OPTIONS="-i external/172.30.140.133!187.191.66.243  -o 60 -a 3600 -d 30 -s 120 -n 172.30.140.133:22222 -m 20000 -M 30000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
+PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+PRIVATE_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/private/0/ipv4/address)
+
+
+echo "OPTIONS="-i $PUBLIC_IPV4  -o 60 -a 3600 -d 30 -s 120 -n $PRIVATE_IPV4:22222 -m 20000 -M 60000 -L 7 --log-facility=local1""  > /etc/rtpengine-config.conf
 systemctl enable rtpengine && systemctl start rtpengine
 
 reboot
